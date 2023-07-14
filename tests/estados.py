160851bc054bc4242a5f43c4e7aa2ab98bbcc1cf
Welinton Passos <welintonpassos2003@gmail.com>
2023-07-14 19:00:34 -0300

    codigo menina WPP

---
+++ tests/estados.py
@@ -0,0 +1,11 @@
+estados = []
+
+estados = ["Espirito Santo", "Minas Gerais", "Rio de Janeiro", "São Paulo"]
+
+print(estados[0])
+
+estados[0] = "Vitória"
+estados[1] = "Belo Horizonte"
+
+for estado in estados:
+    print(estado)
