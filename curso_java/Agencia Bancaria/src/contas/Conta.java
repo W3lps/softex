cc98f4b76764f1f7021b265b4c67e70d0076bcd4
Welinton Passos <welintonpassos2003@gmail.com>
2023-07-21 17:17:05 -0300

    continuando curso de java

--- curso_java/Agencia Bancaria/src/contas/Conta.java
+++ curso_java/Agencia Bancaria/src/contas/Conta.java
@@ -1,14 +1,52 @@
 package contas;
 
 public class Conta {
-	String cliente;
-	double saldo;
+	private String cliente;
 	
-	public Conta() {
+	public String getCliente() {
+		return cliente;
+	}
+	
+	public void setCliente(String cliente) {
+		this.cliente = cliente;
+	}
+	
+	private double saldo;
+	
+	public double getSaldo() {
+		return saldo;
+	}
+	
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	protected Conta() {
 		System.out.println("Agência 0265");
 	}
 	
-	void exibirSaldo() {
+	protected void exibirSaldo() {
 		System.out.println("Saldo: R$:" + saldo);
+	}
+	
+	void sacar(double valor) {
+		saldo -= valor;
+		System.out.println("Débito: R$: " + valor);
+	}
+	
+	void depositar(double valor) {
+		saldo += valor;
+		System.out.println("Crédito: R$: " + valor);
+	}
+	
+	void transferir(Conta destino, double valor) {
+		this.sacar(valor);
+		destino.depositar(valor);
+		System.out.println("Transferência de: " + valor + " Para conta de: " + destino.cliente);
+	}
+	
+	double soma(double cc1, double cc2) {
+		double total = cc1 + cc2;
+		return total;
 	}
 }
--- curso_java/Agencia Bancaria/src/contas/PessoaFisica.java
+++ curso_java/Agencia Bancaria/src/contas/PessoaFisica.java
@@ -4,18 +4,29 @@ public class PessoaFisica {
 
 	public static void main(String[] args) {
 		Conta cc1 = new Conta();
-		cc1.cliente = "Robson Santos";
-		cc1.saldo = 130000;
+		cc1.setCliente("Robson Santos");
+		cc1.setSaldo(130000);
 
-		System.out.println("Cliente: " + cc1.cliente);
+		System.out.println("Cliente: " + cc1.getCliente());
 		cc1.exibirSaldo();
+		cc1.sacar(3000);
 		
 		Conta cc2 = new Conta();
-		cc1.cliente = "Cinara Passos";
-		cc1.saldo = 25000;
+		cc2.setCliente("Cinara Passos");
+		cc2.setSaldo(25000);
+		cc2.depositar(3000);
 
-		System.out.println("Cliente: " + cc1.cliente);
-		cc1.exibirSaldo();
+		System.out.println("Cliente: " + cc2.getCliente());
+		cc2.exibirSaldo();
+		
+		System.out.println("_______________________________");
+		cc1.transferir(cc2, 5000);
+		
+		System.out.println("_______________________________");
+		System.out.println("Relatório: ");
+		Conta gerente = new Conta();
+		double relatorio = gerente.soma(cc1.getSaldo(), cc2.getSaldo());
+		System.out.println("Saldo total das contas: " + relatorio);
 	}
 
 }
